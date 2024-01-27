afba73d9d305c7855975693128118eeae9cc1dd4
Vikrantpatil19 <pvikrant010@gmail.com>
2024-01-27 17:55:56 +0530

    Add the js to seek the song

--- Script.js
+++ Script.js
@@ -2,10 +2,23 @@
 //Global variable for changing the current song
 let currentSong = new Audio();
 
-//The getSongs function do's the work of fetching the songs from the local directory and return the songs.
+//By using the chatgpt - write the function that takes the seconds and convert it into the formate of seconds/minutes
+// Function to convert seconds to minutes and seconds format
+function secondsToMinutesSeconds(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
 
+  // Add leading zeros if necessary
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+
+//The getSongs function do's the work of fetching the songs from the local directory and return the songs.
 async function getSongs() {
-  let a = await fetch("http://127.0.0.1:5500/Songs/");
+  let a = await fetch("http://127.0.0.1:5500/Songs/"); 
   let response = await a.text();
   console.log(response);
   let div = document.createElement("div");
@@ -26,13 +39,21 @@ async function getSongs() {
  
 //creating the function to play the song
 
-const playMusic = (track)=>{
+const playMusic = (track, pause=false)=>{
    //let audio = new Audio("/songs/" + track)
    
    //here we do not making the new audio ,but we changing the current source and then we play the audio
    currentSong.src= "/songs/" + track
-   currentSong.play()
-   play.src = "pause.svg" //firstly it is paused when the song is playing
+   if(!pause){
+    currentSong.play()
+    play.src = "pause.svg" //firstly it is paused when the song is playing
+
+   }
+   
+   
+   //to display the song info and song time
+   document.querySelector(".songInfo").innerHTML = decodeURI(track)
+   document.querySelector(".songTime").innerHTML = "00:00/00:00"
 }
 
 //here the promise is pending so for that we can do this and get the list of all songs
@@ -41,6 +62,7 @@ async function main() {
   
   //get the list of the song
   let songs = await getSongs()
+  playMusic(songs[0] , true)
     
 
     //put the songs in the your library in the ul tag & show all the songs in the playlist
@@ -81,6 +103,27 @@ async function main() {
         currentSong.pause()
         play.src= "play.svg"
       }
+    })
+
+    //Time update event
+    currentSong.addEventListener("timeupdate" , () =>{
+      console.log(currentSong.currentTime , currentSong.duration);
+      document.querySelector(".songTime").innerHTML = 
+      `${secondsToMinutesSeconds(currentSong.currentTime)}/
+      ${secondsToMinutesSeconds(currentSong.duration)}`
+      
+      document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration)*100 + "%";
+
+      //Adding the event listener to seek bar
+
+      document.querySelector(".seekbar").addEventListener("click", e =>{
+
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width )*100 ;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration)*percent)/100
+      })
+       
+     
     })
 
 
